8c7758cbf56a90824e0398753ccc005b070a800f
Ashsudheer <ashwinsudheer@outlook.com>
2020-01-20 17:48:21 +0530

    React implementation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import socketIOClient from "socket.io-client";
+
+class App extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+          endpoint: "http://localhost:4001",
+          data: 0
+        };
+    }
+
+    render() {
+        const socket = socketIOClient(this.state.endpoint);
+        socket.on('data', (value) => {
+            this.setState({data: value})
+            ReactDOM.render(<div className="App">{this.state.data}</div>, document.getElementById('root'));
+        })
+        return (
+            <div className="App">
+                {this.state.data}
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
