1998e1f3c8d1dd73cff4634f3a51d7ef245494d7
Ashsudheer <ashwinsudheer@outlook.com>
2020-01-26 13:00:03 +0530

    cards

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import socketIOClient from "socket.io-client";
+import socketIOClient from "socket.io-client";
 import './App.css';
 import logo from '../img/DR-White.png';
 import { Card } from 'react-bootstrap';
@@ -15,31 +15,38 @@ class App extends React.Component{
     }
 
     render() {
-        // const socket = socketIOClient(this.state.endpoint);
-        // socket.on('data', (value) => {
-        //     this.setState({data: value})
-        //     ReactDOM.render(<div className="output">{value}</div>, document.getElementById('root'));
-        // })
+        const socket = socketIOClient(this.state.endpoint);
+        socket.on('data', (value) => {
+            this.setState({data: value})
+            const element = (
+                <div className="container-fluid App">
+                    <div className="container-fluid image">
+                        <img className="logo" src={logo} alt=""/>
+                    </div>
+                    <Card className="card">
+                        <Card.Body className="card-body">
+                            <Card.Title className="card-text">Data</Card.Title>
+                            <Card.Text className="card-text">
+                                {value}
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+            );
+
+            ReactDOM.render(element, document.getElementById('root'));
+        })
         return (
             <div className="container-fluid App">
                 <div className="container-fluid image">
                     <img className="logo" src={logo} alt=""/>
                 </div>
-                {/* <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Data</h5>
-                        <div class="ouput">
-
-                        </div>
-                    </div>
-                </div> */}
                 <Card className="card">
                     <Card.Body className="card-body">
-                        <Card.Title>Data</Card.Title>
-                        {/* <div className="output">
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </div> */}
+                        <Card.Title className="card-text">Data</Card.Title>
+                        <Card.Text className="card-text">
+
+                        </Card.Text>
                     </Card.Body>
                 </Card>
             </div>
